d7a3327b90c964e3281cfee24ec14b9db7530407
iamsajidshaz-dev <iamsajidshaz@gmail.com>
2026-01-21 13:32:38 +0530

    resolved api key security issue

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -3,6 +3,7 @@
 import 'package:firebase_core/firebase_core.dart' show FirebaseOptions;
 import 'package:flutter/foundation.dart'
     show defaultTargetPlatform, kIsWeb, TargetPlatform;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 /// Default [FirebaseOptions] for use with your Firebase apps.
 ///
@@ -52,11 +53,14 @@ class DefaultFirebaseOptions {
     }
   }
 
-  static const FirebaseOptions android = FirebaseOptions(
-    apiKey: 'AIzaSyDdlB08WVknSrkS1OaHSwcoh7TFPENcL1g',
-    appId: '1:589402839348:android:470729db1dd1064d3b6d61',
-    messagingSenderId: '589402839348',
-    projectId: 'shamazchits-shammu',
-    storageBucket: 'shamazchits-shammu.firebasestorage.app',
+  static final FirebaseOptions android = FirebaseOptions(
+
+    apiKey: dotenv.env['FIREBASE_API_KEY']!,
+  appId: dotenv.env['FIREBASE_APP_ID']!,
+  messagingSenderId: dotenv.env['FIREBASE_MESSAGING_SENDER_ID']!,
+  projectId: dotenv.env['FIREBASE_PROJECT_ID']!,
+  storageBucket: dotenv.env['FIREBASE_STORAGE_BUCKET']!,
   );
 }
+
+
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
 import 'services/auth_service.dart';
 import 'screens/auth/login_screen.dart';
@@ -9,6 +10,7 @@ import 'screens/customer/customer_dashboard.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");  // ADD THIS
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
